cead49c751d8e60cafd970de526f79af61d597b0
k-cielo <skyhn10@gmail.com>
2024-07-07 03:35:27 +0900

    entityRef

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor //생성자 대체 어노테이션
+@ToString//toString()메서드 대체 어노테이션
 @Entity
 public class Article {
     @Id
@@ -18,20 +22,5 @@ public class Article {
     private String content;
 
 
-    //Article 생성자 추가
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
 
-    //toString()메서드 추가
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
