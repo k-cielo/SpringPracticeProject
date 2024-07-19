6a7be9ea247c03f4b577db1f975f88ef76524b27
k-cielo <skyhn10@gmail.com>
2024-07-19 23:55:05 +0900

    TestCode작성

--- src/test/java/com/example/firstproject/service/ArticleServiceTest.java
+++ src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -1,15 +1,20 @@
 package com.example.firstproject.service;
 
+import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import org.junit.jupiter.api.Test;//test패키지 임포트
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*; // 앞으로 사용할 수 있는 패키지 임포트
+
+
+
 
 @SpringBootTest//해당 클래스를 스프링 부트와 연동해 테스트
 class ArticleServiceTest {
@@ -28,5 +33,59 @@ class ArticleServiceTest {
         //3. 비교 및 검증
         assertEquals(expected.toString(), articles.toString());
 
+    }
+
+    @Test
+    void show_성공_존재하는_id_입력() {
+        //1.예상데이터
+        Long id = 1L;
+        Article expected = new Article(id, "가가가가", "1111");
+        //2. 실제데이터
+        Article article = articleService.show(id);
+        //3.비교 및 검증
+        assertEquals(expected.toString(), article.toString());
+
+    }
+
+    @Test
+    void show_실패_존재하지_않는_id_입력(){
+        //1.예상 데이터
+        Long id = -1L;
+        Article expected = null;
+        //2.실제 데이터
+        Article article = articleService.show(id);
+        //3.비교 및 검증
+        assertEquals(expected, article);
+    }
+
+    @Test
+    @Transactional
+    void create_성공_title과_content만_있는_dto_입력() {
+        //1.예상데이터
+        String title = "라라라라";
+        String content = "4444";
+        ArticleForm dto = new ArticleForm(null, title, content);
+        Article expected = new Article(4L, title, content);
+        //2.실제데이터
+        Article article = articleService.create(dto);
+        //3.비교 및 검증
+        assertEquals(expected.toString(), article.toString());
+
+    }
+
+
+    @Test
+    @Transactional
+    void create_실패_id가_포함된_dto_입력() {
+        //1. 예상 데이터
+        Long id = 4L;
+        String title = "라라라라";
+        String content = "4444";
+        ArticleForm dto = new ArticleForm(id, title, content);
+        Article expected = null;
+        //2. 실제 데이터
+        Article article = articleService.create(dto);
+        //3.비교 및 검증
+        assertEquals(expected, article);
     }
 }
